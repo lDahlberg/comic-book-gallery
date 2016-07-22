d46ba176331001cf1d11e0fbcd0111143a851aa6
Luke <lDahlberg@catalystitservices.com>
2016-07-22 08:58:45 -0700

    Added comic book repository

--- src/ComicBookGallery/Controllers/ComicBookController.cs
+++ src/ComicBookGallery/Controllers/ComicBookController.cs
@@ -11,22 +11,9 @@ namespace ComicBookGallery.Controllers
     {
         public ActionResult Detail()
         {
-            var comicBook = new ComicBook()
-            {
-                SeriesTitle = "The Amazing Spiderman",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[] 
-                {
-                    new Models.Artist() { Name ="Dan Slott", Role ="Script" },
-                    new Models.Artist() { Name ="Humberto Ramos", Role ="Pencils" },
-                    new Models.Artist() { Name ="Victor Olazaba", Role ="Inks" },
-                    new Models.Artist() { Name ="Edgar Delgado", Role ="Colors" },
-                    new Models.Artist() { Name ="Chris Eliopoulos", Role ="Letters" },
-                }
-            };
+            
 
-            return View(comicBook);
+            return View();
         }
     }
 }
